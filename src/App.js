8bcc778b5282421c4d0cc31a141ec4fbce953161
Jasserzak <jasserslimani01@gmail.com>
2024-07-12 02:13:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const ChildComponent = ({ name }) => {
+  return <p>Hello, {name}!</p>;
+};
+
+const App = () => {
+  const [name, setName] = useState("World");
+
+  const handleChangeName = () => {
+    setName("React Developer");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Debug Sample App</h1>
+      <ChildComponent name={name} />
+      <button onClick={handleChangeName}>Change Name</button>
     </div>
   );
-}
+};
 
 export default App;
